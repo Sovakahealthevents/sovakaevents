f12b8631a5d3a3ee7652c912c70386e02e1ac5db
Subrahmanya Pavankumar sharma <subrahmanyapavankumarsharma@Subrahmanyas-MacBook-Pro.local>
2026-02-15 17:47:46 +0530

    login

--- app/api/admin-login/route.ts
+++ app/api/admin-login/route.ts
@@ -6,10 +6,11 @@ export async function POST(req: Request) {
   if (password === process.env.ADMIN_PASSWORD) {
     const response = NextResponse.json({ success: true })
 
-    response.cookies.set("admin-auth", "true", {
+   response.cookies.set("admin-auth", "true", {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
       path: "/",
+      maxAge: 60 * 30, // 30 minutes
     })
 
     return response
