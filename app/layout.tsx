16e81ed18ea6d6def163e1120002db0ad12778eb
Subrahmanya Pavankumar sharma <subrahmanyapavankumarsharma@Subrahmanyas-MacBook-Pro.local>
2026-02-19 10:54:47 +0530

    analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import Header from "../components/header";
 import Footer from "../components/footer";
 import SupportCTA from "@/components/SupportCTA";
+import { Analytics } from "@vercel/analytics/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -32,6 +33,7 @@ export default function RootLayout({
       >
         <Header />
         {children}
+        <Analytics />
       </body>
       <SupportCTA/>
       <Footer />
